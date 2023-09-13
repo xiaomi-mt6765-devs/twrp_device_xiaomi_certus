e70d464cb6acca577342327531f1c36b06ff91b4
Hadenix <vsegda_online_007@bk.ru>
2023-09-13 16:46:29 +0300

    certus: establish your authorship of the recovery

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,6 +89,7 @@ PLATFORM_VERSION := 16.1.0
 
 # TWRP Configuration
 TW_THEME := portrait_hdpi
+TW_DEVICE_VERSION := 0 by Hadenix
 TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
